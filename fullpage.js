abe098c48bbdb933dab95131dd4581bc4b91c3f2
AmirK.187 <42927760+AmirK187@users.noreply.github.com>
2019-06-20 23:00:39 +0200

    Add files via upload

---
+++ fullpage.js
@@ -0,0 +1,7 @@
+new fullpage ('#fullpage', {
+    autoScrolling: true,
+    scrollingSpeed: 1300,
+    navigation: true,
+    navigationTooltips: ['Home','Projects','About Me','Contact Info'],
+    showActiveTooltip: true,
+});
